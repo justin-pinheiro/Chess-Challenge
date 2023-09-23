1b681b06babcd328d1e1947c678b394702dc5511
justin <justin.pinheiro@laposte.net>
2023-09-23 13:53:42 +0200

    added openings + refactoring

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -4,38 +4,76 @@ using System;
 public class MyBot : IChessBot
 {
     // Piece values: null, pawn, knight, bishop, rook, queen, king
-    int[] pieceValues = { 0, 100, 300, 300, 500, 900, 10000 };
+    int[] pieceValues = { 0, 1, 3, 3, 5, 9, 10 };
+    int numberOfMovesPlayed = 0;
+    int highestMoveValue = -10;
 
+    string[] whiteOpening1 = { "e2e4", "b1c3", "f1c4", "d2d3", "g1f3"};
+    string[] blackOpening1 = { "f7f6", "b8c6", "e7e5", "f8c5", "g8e7"};
+    
     public Move Think(Board board, Timer timer)
     {
         Move[] allMoves = board.GetLegalMoves();
 
-        // Pick a random move to play if nothing better is found
-        Random rng = new();
-        Move moveToPlay = allMoves[rng.Next(allMoves.Length)];
-        int highestValueCapture = 0;
+        Move moveToPlay = Move.NullMove;
+        
+        string[] opening = GetOpening(board);
 
-        foreach (Move move in allMoves)
+        moveToPlay = GetCheckmateMove(board, allMoves);
+        
+        if (!MoveFound(moveToPlay))
         {
-            // Always play checkmate in one
-            if (MoveIsCheckmate(board, move))
+            foreach (Move move in allMoves)
             {
-                moveToPlay = move;
-                break;
+                if(move.IsCapture)
+                {
+                    // Find highest value capture
+                    Piece capturedPiece = board.GetPiece(move.TargetSquare);
+                    Piece capturingPiece = board.GetPiece(move.StartSquare);
+                    int moveValue = pieceValues[(int)capturedPiece.PieceType] - pieceValues[(int)capturingPiece.PieceType];
+
+                    Console.WriteLine(move + " value : " + moveValue);
+
+                    if (moveValue > highestMoveValue)
+                    {
+                        moveToPlay = move;
+                        highestMoveValue = moveValue;
+                    }
+                }
             }
 
-            // Find highest value capture
-            Piece capturedPiece = board.GetPiece(move.TargetSquare);
-            int capturedPieceValue = pieceValues[(int)capturedPiece.PieceType];
+            highestMoveValue = -10;
+        }
 
-            if (capturedPieceValue > highestValueCapture)
+        if (!MoveFound(moveToPlay))
+        {
+            if (numberOfMovesPlayed < opening.Length)
             {
-                moveToPlay = move;
-                highestValueCapture = capturedPieceValue;
+                // Get opening move based on color and moves played
+                Move openingMove = new Move(opening[numberOfMovesPlayed], board);
+                if(isMoveLegal(openingMove, allMoves)) moveToPlay = openingMove;
             }
         }
 
+        if (!MoveFound(moveToPlay))
+            moveToPlay = GetRandomLegalMove(allMoves);
+
+        numberOfMovesPlayed++;
+        Console.WriteLine(moveToPlay);
         return moveToPlay;
+    }
+
+    // Return true if moveToPlay is not null
+    bool MoveFound(Move moveToPlay)
+    {
+        return !Move.NullMove.Equals(moveToPlay);
+    }
+
+    // Pick a random move to play among legal moves
+    Move GetRandomLegalMove(Move[] allMoves)
+    {
+        Random rng = new();
+        return allMoves[rng.Next(allMoves.Length)];
     }
 
     // Test if this move gives checkmate
@@ -45,5 +83,43 @@ public class MyBot : IChessBot
         bool isMate = board.IsInCheckmate();
         board.UndoMove(move);
         return isMate;
+    }
+
+    // Test if this move gives check
+    bool MoveIsCheck(Board board, Move move)
+    {
+        board.MakeMove(move);
+        bool isCheck = board.IsInCheck();
+        board.UndoMove(move);
+        return isCheck;
+    }
+
+    // if a checkmate is possible, return the move, else return Move.NullMove
+    Move GetCheckmateMove(Board board, Move[] allMoves)
+    {
+        foreach (Move move in allMoves)
+        {
+            if (MoveIsCheckmate(board, move)) return move;
+        }
+        return Move.NullMove;
+    }
+
+    // return true if the move is legal
+    bool isMoveLegal(Move move, Move[] allMoves)
+    {
+        foreach (Move legalMove in allMoves)
+        {
+            if (move.Equals(legalMove))
+            {
+                return true; break;
+            }
+        }
+        return false;
+    }
+
+    string[] GetOpening(Board board)
+    {
+        if (board.IsWhiteToMove) return whiteOpening1;
+        else return blackOpening1;
     }
 }
